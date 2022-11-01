97f03eff6a9e9252b1ce2aab988ba5ac5d8efd19
Emmanuel Kamala <emmanuelkamala86@gmail.com>
2022-11-02 01:40:59 +0300

    Create a hotel component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
+import Hotel from './pages/hotel/Hotel';
 import List from './pages/list/List';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
       </Routes>
     </BrowserRouter>
   );
